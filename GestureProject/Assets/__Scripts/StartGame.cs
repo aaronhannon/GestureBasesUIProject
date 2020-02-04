4536ecad54a6d7f248db63ff2d60522ddfefbbe0
aaronhannon <aaronchannon98@gmail.com>
2020-02-04 20:04:59 +0000

    Basic menu ,Character animations and start button added

---
+++ GestureProject/Assets/__Scripts/StartGame.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGame : MonoBehaviour
+{
+
+    private bool gameStarted = false;
+    private GameObject mainCamera;
+    private GameObject player;
+    public Animator animator;
+    void Start()
+    {
+        
+        mainCamera = GameObject.Find("Main Camera");
+        player = GameObject.Find("Low Poly Warrior");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameStarted == true)
+        {
+            if(mainCamera.transform.position.x < -10)
+            {
+                mainCamera.transform.position = new Vector3(mainCamera.transform.position.x+0.01f, mainCamera.transform.position.y, mainCamera.transform.position.z);
+            }
+            else
+            {
+                animator.SetBool("Started", true);
+                player.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z + 0.1f);
+            }
+
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        gameStarted = true;
+        Debug.Log("Game Started");
+    }
+}
