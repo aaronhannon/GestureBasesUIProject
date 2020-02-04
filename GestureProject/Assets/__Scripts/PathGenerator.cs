2f5bba5ead78b490bd753b08631e7ec83c1bc70b
aaronhannon <aaronchannon98@gmail.com>
2020-02-04 22:31:45 +0000

    Pathway Generator working

---
+++ GestureProject/Assets/__Scripts/PathGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathGenerator : MonoBehaviour
+{
+    public GameObject pathPrefab;
+    void Start()
+    {
+        GameObject copy = pathPrefab;
+
+        
+
+        for (int i = -60; i < -40; i += 2)
+        {
+
+            copy.transform.localScale = new Vector3(Random.Range(1.0f, 2.0f), 1, Random.Range(1.0f, 2.0f));
+            Instantiate(copy, new Vector3(Random.Range(-10.5f, -9.5f), Random.Range(-10.5f, -9.5f), i), Quaternion.identity);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
