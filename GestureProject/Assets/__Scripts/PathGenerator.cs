8640b27d35392d9516a3064e74ed14fdbe6e4ecc
aaronhannon <aaronchannon98@gmail.com>
2020-02-09 19:47:25 +0000

    Converted Grass prefab to fbx format

--- GestureProject/Assets/__Scripts/PathGenerator.cs
+++ GestureProject/Assets/__Scripts/PathGenerator.cs
@@ -8,16 +8,16 @@ public class PathGenerator : MonoBehaviour
     void Start()
     {
         GameObject copy = pathPrefab;
-        GameObject grass = Resources.Load("grass") as GameObject;
+        GameObject grass;
 
 
         for (int i = -60; i < 0; i += 2)
         {
             //Different grass prefabs can be used randomly but Coords need to be fixed
-            grass = Resources.Load("grass1") as GameObject;
+            grass = Resources.Load("grass4") as GameObject;
             copy.transform.localScale = new Vector3(Random.Range(1.5f, 3.0f), 1, Random.Range(1.0f, 2.0f));
             Instantiate(copy, new Vector3(Random.Range(-10.5f, -9.5f), Random.Range(-11f, -10.5f), i), Quaternion.identity);
-            Instantiate(grass, new Vector3(Random.Range(36f, 41f), Random.Range(-12f, -11f), i+ Random.Range(10, 15)), Quaternion.identity);
+            Instantiate(grass, new Vector3(Random.Range(-18f, -15f), Random.Range(-10f, -11f), i+ Random.Range(10, 15)), Quaternion.identity);
         }
 
     }
