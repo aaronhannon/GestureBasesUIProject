39c50daf14f33ff6dc998de7b00b427ce2b71191
aaronhannon <aaronchannon98@gmail.com>
2020-02-09 19:02:47 +0000

    Random Grass being generated along side the pathway

--- GestureProject/Assets/__Scripts/PathGenerator.cs
+++ GestureProject/Assets/__Scripts/PathGenerator.cs
@@ -8,14 +8,16 @@ public class PathGenerator : MonoBehaviour
     void Start()
     {
         GameObject copy = pathPrefab;
+        GameObject grass = Resources.Load("grass") as GameObject;
 
-        
 
         for (int i = -60; i < 0; i += 2)
         {
-
+            //Different grass prefabs can be used randomly but Coords need to be fixed
+            grass = Resources.Load("grass1") as GameObject;
             copy.transform.localScale = new Vector3(Random.Range(1.5f, 3.0f), 1, Random.Range(1.0f, 2.0f));
             Instantiate(copy, new Vector3(Random.Range(-10.5f, -9.5f), Random.Range(-11f, -10.5f), i), Quaternion.identity);
+            Instantiate(grass, new Vector3(Random.Range(36f, 41f), Random.Range(-12f, -11f), i+ Random.Range(10, 15)), Quaternion.identity);
         }
 
     }
