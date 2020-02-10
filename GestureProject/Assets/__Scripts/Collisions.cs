a1a00c91ef092aef76704f60c96a6770fdcd315c
aaronhannon <aaronchannon98@gmail.com>
2020-02-10 22:53:20 +0000

    Lives System added

---
+++ GestureProject/Assets/__Scripts/Collisions.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Collisions : MonoBehaviour
+{
+    private int lives = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("obstacle"))
+        {
+            
+            GameObject heart = GameObject.Find("heart" + lives);
+            Destroy(heart);
+            
+            lives--;
+
+            if (lives == 0)
+            {
+                SceneManager.LoadScene(0);
+                Debug.Log("GameOver");
+            }
+
+            
+        }
+    }
+}
