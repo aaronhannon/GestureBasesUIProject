987b8e6d390f68ca21a74904e0da848234101b1c
Matthew Sloyan <g00348036@gmit.ie>
2020-02-10 18:16:57 +0000

    Added basic player jump.

--- GestureProject/Assets/__Scripts/StartGame.cs
+++ GestureProject/Assets/__Scripts/StartGame.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class StartGame : MonoBehaviour
 {
-
     private bool gameStarted = false;
+    private float jumpSpeed = 4.5f;
     private GameObject mainCamera;
     private GameObject player;
+    private Rigidbody playerRb;
     public Animator animator;
+
     void Start()
     {
-        
         mainCamera = GameObject.Find("Main Camera");
         player = GameObject.Find("Low Poly Warrior");
+        playerRb = player.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -30,10 +32,20 @@ public class StartGame : MonoBehaviour
                 animator.SetBool("Started", true);
                 player.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z + 0.2f);
             }
-
+            // Check if player wants to jump.
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                PlayerJump();
+            }
         }
     }
-
+    
+    // Make the player jump.
+    private void PlayerJump()
+    {
+        playerRb.AddForce(new Vector3(0.0f, 2.0f, 0.0f) * jumpSpeed, ForceMode.Impulse);
+    }
+    
     private void OnMouseDown()
     {
         gameStarted = true;
