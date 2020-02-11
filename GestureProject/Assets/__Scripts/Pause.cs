388cb21e93c9e97a8adb58090428d522e5fe5cc5
aaronhannon <aaronchannon98@gmail.com>
2020-02-11 18:26:23 +0000

    ESC key triggers pause menu animation

---
+++ GestureProject/Assets/__Scripts/Pause.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    private GameObject camera;
+    private Animator anim;
+    private bool paused = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = GameObject.Find("Main Camera");
+        anim = camera.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            if(paused == false) {
+                paused = true;
+                anim.SetBool("Paused", true);
+            }
+            else
+            {
+                paused = false;
+                anim.SetBool("Paused", false);
+            }
+                
+        }
+    }
+}
