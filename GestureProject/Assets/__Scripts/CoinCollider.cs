cd3e5a3547db8997451b8d13dc25fd6f7580383c
michaelcoleman7 <michaelcoleman135@gmail.com>
2020-02-13 13:42:04 +0000

    Added coin collision and animation

---
+++ GestureProject/Assets/__Scripts/CoinCollider.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinCollider : MonoBehaviour
+{
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+
+        if (other.CompareTag("Player"))
+        {
+            print("Collide found");
+            print(gameObject.GetComponent<Animator>().GetBool("pickup"));
+            gameObject.GetComponent<Animator>().SetBool("pickup", true);
+            print(gameObject.GetComponent<Animator>().GetBool("pickup"));
+        }
+    }
+}
