76f125c3a841cd8b71cea8b32cccd90e9750a95c
aaronhannon <aaronchannon98@gmail.com>
2020-02-13 11:57:41 +0000

    Helm powerup fully implemented

--- GestureProject/Assets/__Scripts/Collisions.cs
+++ GestureProject/Assets/__Scripts/Collisions.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class Collisions : MonoBehaviour
 {
     private int lives = 3;
+    private bool helmet = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,24 +26,36 @@ public class Collisions : MonoBehaviour
 
             other.gameObject.GetComponent<Animator>().SetBool("collided", true);
 
-            GameObject heart = GameObject.Find("heart" + lives);
-            Animator a = heart.GetComponent<Animator>();
-            a.SetBool("Destroyed", true);
-            //Destroy(heart);
-            
-            lives--;
-
-            if (lives == 0)
+            if(helmet == false)
             {
-                SceneManager.LoadScene(0);
-                Debug.Log("GameOver");
+                GameObject heart = GameObject.Find("heart" + lives);
+                Animator a = heart.GetComponent<Animator>();
+                a.SetBool("Destroyed", true);
+                //Destroy(heart);
+
+                lives--;
+
+                if (lives == 0)
+                {
+                    SceneManager.LoadScene(0);
+                    Debug.Log("GameOver");
+                }
             }
+            else
+            {
+                GameObject.Find("helmPowerUp").GetComponent<Animator>().SetBool("spawn", false);
+                GameObject.Find("playerHelm").GetComponent<Animator>().SetBool("spawnHelm", false);
+                helmet = false;
+            }
+
 
             
         }else if (other.CompareTag("helmet"))
         {
             // X size = 0.003000001 y size = 0.004000003 z size = 0.003600002
             GameObject.Find("playerHelm").GetComponent<Animator>().SetBool("spawnHelm",true);
+            GameObject.Find("helmPowerUp").GetComponent<Animator>().SetBool("spawn", true);
+            helmet = true;
         }
     }
 }
