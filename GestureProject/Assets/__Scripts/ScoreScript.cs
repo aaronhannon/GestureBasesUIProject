53b12512c88ea91b84884b41107e721a350be8b7
Michael Coleman <michaelcoleman135@gmail.com>
2020-02-14 00:34:45 +0000

    Added scoring system which increases with player z coordinates

---
+++ GestureProject/Assets/__Scripts/ScoreScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ScoreScript : MonoBehaviour
+{
+    public TextMeshPro TextMeshPro;
+    public Camera player;
+    private int score = -1;
+    private Vector3 _lastPosition;
+    void Start()
+    {
+        _lastPosition = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((this.transform.position.z - _lastPosition.z) > 0)
+        {
+            score++;
+        }
+       
+
+        _lastPosition = this.transform.position;
+        TextMeshPro.text = "Score: "+score.ToString();
+    }
+}
