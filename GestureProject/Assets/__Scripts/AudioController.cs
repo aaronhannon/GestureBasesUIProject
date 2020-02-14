df6f24a309d47264c2634172a9a1a2c50ad5cf36
Matthew Sloyan <g00348036@gmit.ie>
2020-02-14 17:14:05 +0000

    Setup AudioController script and set up Singleton.

---
+++ GestureProject/Assets/__Scripts/AudioController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    #region == Private Variables == 
+
+    private AudioSource source;
+
+    #endregion
+
+    // Singleton design pattern to get instance of class
+    public static AudioController Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        // Get AudioSource component and start background music playing.
+        source = GetComponent<AudioSource>();
+    }
+}
