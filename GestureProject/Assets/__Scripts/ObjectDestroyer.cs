5a5db4c8c06a3b3dc06aa726b0539d023ce7fc8e
Michael Coleman <michaelcoleman135@gmail.com>
2020-02-15 22:24:09 +0000

    Added object destoryer which upon player passing destroys objects

---
+++ GestureProject/Assets/__Scripts/ObjectDestroyer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectDestroyer : MonoBehaviour
+{
+    private GameObject destructionPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Find object by name so doesnt need attachment to each platform;
+        destructionPoint = GameObject.Find("ObjDestructionTransform");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if platforms z position is less than the destruction points z position.
+        if (transform.position.z < destructionPoint.transform.position.z)
+        {
+            //Destroy the object the script is attached to
+            Destroy(gameObject);
+        }
+    }
+}
