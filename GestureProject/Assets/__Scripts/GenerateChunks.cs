8ac25944aa200ba4023bfe68dd5cb759e6d2b032
aaronhannon <aaronchannon98@gmail.com>
2020-02-16 19:21:54 +0000

    Chunks randomly generating

---
+++ GestureProject/Assets/__Scripts/GenerateChunks.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateChunks : MonoBehaviour
+{
+    GameObject villageChunk;
+    GameObject forestChunk;
+    float currentZ= 0 ;
+    string previousChunk;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        villageChunk = Resources.Load("VillageChunk") as GameObject;
+        forestChunk = Resources.Load("ForestChunk") as GameObject;
+        for (int i = 0; i < 4; i++)
+        {
+            int rand = Random.Range(0, 2);
+            Debug.Log("RANDOM: " + rand);
+            if(rand == 0)
+            {
+                if(previousChunk == "forest")
+                {
+                    currentZ += 450f;
+                }
+                else
+                {
+                    currentZ += 350f;
+                }
+                
+                Instantiate(villageChunk, new Vector3(-18f, 1f, currentZ), Quaternion.Euler(0, 90, 0));
+                previousChunk = "village";
+            }
+            else
+            {
+
+                currentZ += 230;
+                Instantiate(forestChunk, new Vector3(-22f, 0f, currentZ), Quaternion.Euler(0, 90, 0));
+                previousChunk = "forest";
+            }
+
+
+        }
+
+        //spawn village Add 360 every time to Z coord
+
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- GestureProject/Assets/__Scripts/StartGame.cs
+++ GestureProject/Assets/__Scripts/StartGame.cs
@@ -14,7 +14,7 @@ public class StartGame : MonoBehaviour
 
     private bool gameStarted = false;
     private float jumpSpeed = 4.5f;
-    private float playerSpeed = 0.2f;
+    private float playerSpeed = 3f;
     private float moveSpeed = 0.5f;
     private float distToGround;
     private GameObject mainCamera;
