3e37e2d073519743aca25661bca4ee4374faeaca
Matthew Sloyan <g00348036@gmit.ie>
2020-02-22 14:08:31 +0000

    Added randomly spawning logs.

--- GestureProject/Assets/__Scripts/ObstacleGenerator.cs
+++ GestureProject/Assets/__Scripts/ObstacleGenerator.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class ObstacleGenerator : MonoBehaviour
 {
     // Single object for now, could use a list later.
-    private GameObject fence;
+    private GameObject obstacle;
     private GameObject fallingTree;
     private GameObject npc;
     private GameObject coin;
@@ -17,14 +17,25 @@ public class ObstacleGenerator : MonoBehaviour
         container = GameObject.Find("GeneratedObjects");
 
         // Basic implementation for now, will need to be improved and randomized.
-        for (int i = 0; i < 240; i += 25)
+        for (int i = 0; i < 400; i += 27)
         {
-            fence = Resources.Load("fence") as GameObject;
-            Instantiate(fence, new Vector3(Random.Range(-3f, 3f), 1.5f, i), Quaternion.identity).transform.parent = container.transform;
+            int rand = Random.Range(0, 2);
+
+            // Load either logs or fences
+            if (rand == 0)
+            {
+                obstacle = Resources.Load("fence") as GameObject;
+            }
+            else
+            {
+                obstacle = Resources.Load("logs") as GameObject;
+            }
+
+            Instantiate(obstacle, new Vector3(Random.Range(-3f, 3f), 1.5f, i), Quaternion.identity).transform.parent = container.transform;
         }
 
         // Basic implementation for now, will need to be improved and randomized.
-        for (int i = 100; i < 800; i += 50)
+        for (int i = 100; i < 500; i += 50)
         {
             npc = Resources.Load("NPC_Man") as GameObject;
             Instantiate(npc, new Vector3(Random.Range(-3f, 3f), 1f, i), Quaternion.Euler(0, -180, 0)).transform.parent = container.transform;
@@ -34,7 +45,7 @@ public class ObstacleGenerator : MonoBehaviour
         for (int i = 50; i < 450; i += 100)
         {
             fallingTree = Resources.Load("TriggerObstacle") as GameObject;
-            Instantiate(fallingTree, new Vector3(0.2f, 2.5f, i), Quaternion.identity).transform.parent = container.transform;
+            Instantiate(fallingTree, new Vector3(0.5f, 2.5f, i), Quaternion.identity).transform.parent = container.transform;
         }
 
         // Basic implementation for now, will need to be improved and randomized.
