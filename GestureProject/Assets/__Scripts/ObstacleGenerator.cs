00950f2ae1246b838a32272e44df3d5b948e446d
Matthew Sloyan <g00348036@gmit.ie>
2020-02-23 15:35:48 +0000

    Redesigned obstactle generator script to make it more dynamic for chunk generation.

--- GestureProject/Assets/__Scripts/ObstacleGenerator.cs
+++ GestureProject/Assets/__Scripts/ObstacleGenerator.cs
@@ -1,9 +1,9 @@
-﻿
-using UnityEngine;
+﻿using UnityEngine;
 
 public class ObstacleGenerator : MonoBehaviour
 {
-    private static int numberOfChunks = 0;
+    private static int numberOfChunks = 1;
+    private const int chunkLenght = 500;
 
     // Single object for now, could use a list later.
     private GameObject obstacle;
@@ -11,19 +11,34 @@ public class ObstacleGenerator : MonoBehaviour
     private GameObject rock;
     private GameObject npc;
     private GameObject coin;
-    private GameObject container;
     private GameObject revivePotion;
+
+    private GameObject container;
 
     void Start()
     {
+        container = GameObject.Find("GeneratedObjects");
+
         GenerateVillageObjects();
         GenerateRiverObjects();
         //GenerateForestObjects();
+    }
 
-        container = GameObject.Find("GeneratedObjects");
+    
+    #region == Chunk Object Generators == 
+    public void GenerateVillageObjects()
+    {
+        int start = (numberOfChunks - 1) * 400;
+        int end = numberOfChunks * 400;
+
+        for (int i = start; i < end; i += 50)
+        {
+            npc = Resources.Load("NPC_Man") as GameObject;
+            Instantiate(npc, new Vector3(Random.Range(-3f, 3f), 1f, i), Quaternion.Euler(0, -180, 0)).transform.parent = container.transform;
+        }
 
         // Basic implementation for now, will need to be improved and randomized.
-        for (int i = 30; i < 400; i += 27)
+        for (int i = start; i < end; i += 27)
         {
             int rand = Random.Range(0, 2);
 
@@ -40,87 +55,83 @@ public class ObstacleGenerator : MonoBehaviour
             Instantiate(obstacle, new Vector3(Random.Range(-3f, 3f), 1.5f, i), Quaternion.identity).transform.parent = container.transform;
         }
 
-        SpawnNPCS();
-        SpawnTrees();
-        SpawnCoins();
-        SpawnHelmets();
-        SpawnPotions();
-
-        // Basic implementation for now, will need to be improved and randomized.
-        for (int i = 500; i < 900; i += 55)
-        {
-            rock = Resources.Load("rock") as GameObject;
-            Instantiate(rock, new Vector3(Random.Range(-4f, 4f), 1.2f, i), Quaternion.identity).transform.parent = container.transform;
-        }
-
-        // Basic implementation for now, will need to be improved and randomized.
-        //for (int i = 100; i < 500; i += 50)
-        //{
-        //    npc = Resources.Load("NPC_Man") as GameObject;
-        //    Instantiate(npc, new Vector3(Random.Range(-3f, 3f), 1f, i), Quaternion.Euler(0, -180, 0)).transform.parent = container.transform;
-        //}
-
-        // Basic implementation for now, will need to be improved and randomized.
-        //for (int i = 50; i < 500; i += 150)
+        // Testing to show trees for now. Will be moved to forest.
+        //for (int i = start; i < end; i += 150)
         //{
         //    fallingTree = Resources.Load("TriggerObstacle") as GameObject;
         //    Instantiate(fallingTree, new Vector3(0.5f, 2.5f, i), Quaternion.identity).transform.parent = container.transform;
         //}
-
-        // Basic implementation for now, will need to be improved and randomized.
-        for (int i = 30; i < 300; i += 50)
-        {
-            coin = Resources.Load("coin") as GameObject;
-            Instantiate(coin, new Vector3(Random.Range(-3f, 3f), 1.5f, i), Quaternion.identity).transform.parent = container.transform;
-        }
-
-    }
-
-    #region == Chunk Object Generators == 
-    public void GenerateVillageObjects()
-    {
+        
+        SpawnCoins(start, end);
+        SpawnPotions(start, end);
 
         numberOfChunks++;
     }
 
     public void GenerateForestObjects()
     {
+        int start = (numberOfChunks - 1) * 400;
+        int end = numberOfChunks * 400;
+
+        for (int i = start; i < end; i += 150)
+        {
+            fallingTree = Resources.Load("TriggerObstacle") as GameObject;
+            Instantiate(fallingTree, new Vector3(0.5f, 2.5f, i), Quaternion.identity).transform.parent = container.transform;
+        }
+
+        SpawnCoins(start, end);
+        SpawnPotions(start, end);
 
         numberOfChunks++;
     }
 
     public void GenerateRiverObjects()
     {
+        Debug.Log(numberOfChunks);
+
+        int start = (numberOfChunks - 1) * 400;
+        Debug.Log(start);
+
+        int end = numberOfChunks * 400;
+        Debug.Log(end);
+
+        for (int i = start; i < end; i += 55)
+        {
+            rock = Resources.Load("rock") as GameObject;
+            Instantiate(rock, new Vector3(Random.Range(-4f, 4f), 1.2f, i), Quaternion.identity).transform.parent = container.transform;
+        }
+
+        SpawnCoins(start, end);
+        SpawnPotions(start, end);
 
         numberOfChunks++;
     }
     #endregion
 
     #region == Single Object Spawners == 
+   
     private void SpawnHelmets()
     {
+
     }
 
-    private void SpawnPotions()
+    private void SpawnPotions(int start, int end)
     {
         // Basic implementation for now, will need to be improved and randomized.
-        for (int i = 30; i < 300; i += 100)
+        for (int i = start; i < end; i += 100)
         {
             revivePotion = Resources.Load("RevivePotion") as GameObject;
             Instantiate(revivePotion, new Vector3(Random.Range(-3f, 3f), 1.5f, i), Quaternion.identity).transform.parent = container.transform;
         }
     }
 
-    private void SpawnCoins()
+    private void SpawnCoins(int start, int end)
     {
-    }
-
-    private void SpawnTrees()
-    {
-    }
-
-    private void SpawnNPCS()
-    {
+        for (int i = start ; i < end; i += 50)
+        {
+            coin = Resources.Load("coin") as GameObject;
+            Instantiate(coin, new Vector3(Random.Range(-3f, 3f), 1.5f, i), Quaternion.identity).transform.parent = container.transform;
+        }
     }
     #endregion
 }
