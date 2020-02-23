5549cca2dd7862683aced67926bdde829d008368
Matthew Sloyan <g00348036@gmit.ie>
2020-02-23 16:02:06 +0000

    Further improved design of Obstacle generator class, chunk objects can now be spawned dynamically.

--- GestureProject/Assets/__Scripts/ObstacleGenerator.cs
+++ GestureProject/Assets/__Scripts/ObstacleGenerator.cs
@@ -3,13 +3,18 @@
 public class ObstacleGenerator : MonoBehaviour
 {
     private static int numberOfChunks = 1;
-    private const int chunkLenght = 500;
+    private const int chunkLenghtVillage = 420;
+    private const int chunkLenghtRiver = 520;
+    private const int chunkLenghtForest = 500;
+    private int start = 0;
+    private int end = 0;
 
     // Single object for now, could use a list later.
-    private GameObject obstacle;
-    private GameObject fallingTree;
-    private GameObject rock;
+    private GameObject fence;
     private GameObject npc;
+    private GameObject fallingTree;
+    private GameObject logs;
+    private GameObject rock;
     private GameObject coin;
     private GameObject revivePotion;
 
@@ -19,49 +24,53 @@ public class ObstacleGenerator : MonoBehaviour
     {
         container = GameObject.Find("GeneratedObjects");
 
+        // Testing, will be called randomly from chunk generator.
         GenerateVillageObjects();
         GenerateRiverObjects();
-        //GenerateForestObjects();
+        GenerateForestObjects();
     }
 
-    
     #region == Chunk Object Generators == 
     public void GenerateVillageObjects()
     {
-        int start = (numberOfChunks - 1) * 400;
-        int end = numberOfChunks * 400;
+        // Get start and end values to load objects from, so it aligns with each chunk.
+        UpdateStartEndValues(chunkLenghtVillage);
 
-        for (int i = start; i < end; i += 50)
+        for (int i = start; i < end; i += 60)
         {
             npc = Resources.Load("NPC_Man") as GameObject;
             Instantiate(npc, new Vector3(Random.Range(-3f, 3f), 1f, i), Quaternion.Euler(0, -180, 0)).transform.parent = container.transform;
         }
-
-        // Basic implementation for now, will need to be improved and randomized.
-        for (int i = start; i < end; i += 27)
+        
+        for (int i = start + 30; i < end; i += 27)
         {
-            int rand = Random.Range(0, 2);
-
-            // Load either logs or fences
-            if (rand == 0)
-            {
-                obstacle = Resources.Load("fence") as GameObject;
-            }
-            else
-            {
-                obstacle = Resources.Load("logs") as GameObject;
-            }
-
-            Instantiate(obstacle, new Vector3(Random.Range(-3f, 3f), 1.5f, i), Quaternion.identity).transform.parent = container.transform;
+            fence = Resources.Load("fence") as GameObject;
+            Instantiate(fence, new Vector3(Random.Range(-3f, 3f), 1.5f, i), Quaternion.identity).transform.parent = container.transform;
         }
 
-        // Testing to show trees for now. Will be moved to forest.
+        // Testing to show trees.
         //for (int i = start; i < end; i += 150)
         //{
         //    fallingTree = Resources.Load("TriggerObstacle") as GameObject;
         //    Instantiate(fallingTree, new Vector3(0.5f, 2.5f, i), Quaternion.identity).transform.parent = container.transform;
         //}
-        
+
+        SpawnCoins(start, end);
+        SpawnPotions(start, end);
+
+        numberOfChunks++;
+    }
+    
+    public void GenerateRiverObjects()
+    {
+        UpdateStartEndValues(chunkLenghtRiver);
+
+        for (int i = start + 30; i < end; i += 55)
+        {
+            rock = Resources.Load("rock") as GameObject;
+            Instantiate(rock, new Vector3(Random.Range(-4f, 4f), 1.2f, i), Quaternion.identity).transform.parent = container.transform;
+        }
+
         SpawnCoins(start, end);
         SpawnPotions(start, end);
 
@@ -70,35 +79,18 @@ public class ObstacleGenerator : MonoBehaviour
 
     public void GenerateForestObjects()
     {
-        int start = (numberOfChunks - 1) * 400;
-        int end = numberOfChunks * 400;
+        UpdateStartEndValues(chunkLenghtForest);
 
-        for (int i = start; i < end; i += 150)
+        for (int i = start + 30; i < end; i += 27)
+        {
+            logs = Resources.Load("logs") as GameObject;
+            Instantiate(logs, new Vector3(Random.Range(-3f, 3f), 1.5f, i), Quaternion.identity).transform.parent = container.transform;
+        }
+
+        for (int i = start + 30; i < end; i += 150)
         {
             fallingTree = Resources.Load("TriggerObstacle") as GameObject;
             Instantiate(fallingTree, new Vector3(0.5f, 2.5f, i), Quaternion.identity).transform.parent = container.transform;
-        }
-
-        SpawnCoins(start, end);
-        SpawnPotions(start, end);
-
-        numberOfChunks++;
-    }
-
-    public void GenerateRiverObjects()
-    {
-        Debug.Log(numberOfChunks);
-
-        int start = (numberOfChunks - 1) * 400;
-        Debug.Log(start);
-
-        int end = numberOfChunks * 400;
-        Debug.Log(end);
-
-        for (int i = start; i < end; i += 55)
-        {
-            rock = Resources.Load("rock") as GameObject;
-            Instantiate(rock, new Vector3(Random.Range(-4f, 4f), 1.2f, i), Quaternion.identity).transform.parent = container.transform;
         }
 
         SpawnCoins(start, end);
@@ -118,7 +110,7 @@ public class ObstacleGenerator : MonoBehaviour
     private void SpawnPotions(int start, int end)
     {
         // Basic implementation for now, will need to be improved and randomized.
-        for (int i = start; i < end; i += 100)
+        for (int i = start; i < end; i += 150)
         {
             revivePotion = Resources.Load("RevivePotion") as GameObject;
             Instantiate(revivePotion, new Vector3(Random.Range(-3f, 3f), 1.5f, i), Quaternion.identity).transform.parent = container.transform;
@@ -134,4 +126,11 @@ public class ObstacleGenerator : MonoBehaviour
         }
     }
     #endregion
+    
+    private void UpdateStartEndValues(int chunkLenght)
+    {
+        // Get start and end values to load objects from, so it aligns with each chunk.
+        start = (numberOfChunks - 1) * chunkLenght;
+        end = numberOfChunks * chunkLenght;
+    }
 }
