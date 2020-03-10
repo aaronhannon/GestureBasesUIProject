7dda7d65d4ce3d2a8371afd947abe947278978a5
Matthew Sloyan <g00348036@gmit.ie>
2020-03-10 19:56:37 +0000

    Added voice control script and set up play game method for testing.

---
+++ GestureProject/Assets/__Scripts/VoiceControl.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class VoiceControl : MonoBehaviour
+{
+    private KeywordRecognizer speechRecognizer;
+    private Dictionary<string, Action> voiceActions = new Dictionary<string, Action>();
+    
+    void Start()
+    {
+        voiceActions.Add("start game", PlayGame);
+
+        speechRecognizer = new KeywordRecognizer(voiceActions.Keys.ToArray());
+        speechRecognizer.OnPhraseRecognized += SpeechRecognizer_OnPhraseRecognized;
+        speechRecognizer.Start();
+    }
+
+    private void SpeechRecognizer_OnPhraseRecognized(PhraseRecognizedEventArgs speech)
+    {
+        voiceActions[speech.text].Invoke();
+    }
+
+    private void PlayGame()
+    {
+        Debug.Log("Start Game");
+    }
+}
