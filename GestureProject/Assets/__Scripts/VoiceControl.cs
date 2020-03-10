08bf2750450ed4a20ebb6b55642298494531d3c7
Matthew Sloyan <g00348036@gmit.ie>
2020-03-10 20:53:01 +0000

    Added comments and refactored code. Need to test on Kinect.

--- GestureProject/Assets/__Scripts/VoiceControl.cs
+++ GestureProject/Assets/__Scripts/VoiceControl.cs
@@ -8,25 +8,31 @@ using UnityEngine.Windows.Speech;
 
 public class VoiceControl : MonoBehaviour
 {
+    #region == Private variables ==
     private KeywordRecognizer speechRecognizer;
     private Dictionary<string, Action> voiceActions = new Dictionary<string, Action>();
     private StartGame startgame;
     private Pause pause;
     private Options options;
+    #endregion
 
     void Start()
     {
+        // Get scripts to access methods.
         startgame = gameObject.GetComponent<StartGame>();
         pause = GameObject.Find("Main Camera").GetComponent<Pause>();
         options = GameObject.Find("VikingSoundHorn").GetComponent<Options>();
 
+        // All all voice commands to Dictionary
         AddAllVoiceCommands();
 
+        // Add keywords from dictionary to KeywordRecognizer and start listening for commands.
         speechRecognizer = new KeywordRecognizer(voiceActions.Keys.ToArray());
         speechRecognizer.OnPhraseRecognized += SpeechRecognizer_OnPhraseRecognized;
         speechRecognizer.Start();
     }
 
+    // On Phrase detected call the method if the command is found.
     private void SpeechRecognizer_OnPhraseRecognized(PhraseRecognizedEventArgs speech)
     {
         voiceActions[speech.text].Invoke();
@@ -34,27 +40,32 @@ public class VoiceControl : MonoBehaviour
 
     private void AddAllVoiceCommands()
     {
+        // Start game
         voiceActions.Add("start game", StartGame);
         voiceActions.Add("play game", StartGame);
         voiceActions.Add("start", StartGame);
         voiceActions.Add("play", StartGame);
 
+        // Pause game
         voiceActions.Add("pause game", PauseGame);
         voiceActions.Add("unpause game", PauseGame);
         voiceActions.Add("pause", PauseGame);
         voiceActions.Add("unpause", PauseGame);
 
+        // Turn sound on/off
         voiceActions.Add("sound", ChangeSound);
         voiceActions.Add("sound on", ChangeSound);
         voiceActions.Add("sound off", ChangeSound);
         voiceActions.Add("turn on sound", ChangeSound);
         voiceActions.Add("turn off sound", ChangeSound);
 
+        // Reset game
         voiceActions.Add("reset game", ResetGame);
         voiceActions.Add("restart game", ResetGame);
         voiceActions.Add("reset", ResetGame);
         voiceActions.Add("restart", ResetGame);
 
+        // Exit game
         voiceActions.Add("exit game", ExitGame);
         voiceActions.Add("close game", ExitGame);
         voiceActions.Add("exit", ExitGame);
@@ -63,11 +74,13 @@ public class VoiceControl : MonoBehaviour
 
     private void StartGame()
     {
+        // Call script to start game.
         startgame.OnMouseDown();
     }
 
     private void PauseGame()
     {
+        // Call script to pause game.
         pause.PauseGame();
     }
     
@@ -84,6 +97,7 @@ public class VoiceControl : MonoBehaviour
 
     private void ChangeSound()
     {
+        // Turn sound on or off.
         options.OnMouseDown();
     }
 }
