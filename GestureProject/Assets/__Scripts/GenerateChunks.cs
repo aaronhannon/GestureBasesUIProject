4044ada0cba8dfa87eb3aae948fe88fb84b27190
aaronhannon <aaronchannon98@gmail.com>
2020-03-20 16:45:30 +0000

    Changes to Chunk Generator

--- GestureProject/Assets/__Scripts/GenerateChunks.cs
+++ GestureProject/Assets/__Scripts/GenerateChunks.cs
@@ -11,13 +11,15 @@ public class GenerateChunks : MonoBehaviour
     float currentZ = 0;
     string previousChunk;
     ArrayList chunks;
+    ArrayList chunkObjects;
 
     // Start is called before the first frame update
     void Start()
     {
         chunks = new ArrayList();
+        chunkObjects = new ArrayList();
         //Hardcode order you want to test chunks
-        int[] ChunkPatterns = { 1, 2, 0,2 };
+        int[] ChunkPatterns = { 0, 2, 1 };
         
         villageChunk = Resources.Load("VillageChunk") as GameObject;
         forestChunk = Resources.Load("ForestChunk") as GameObject;
@@ -31,72 +33,22 @@ public class GenerateChunks : MonoBehaviour
             //VILLAGE
             if (rand == 0)
             {
-                if (previousChunk == "forest")
-                {
-                    currentZ += 450f;
-                }
-                else if (previousChunk == "river")
-                {
-                    currentZ += 850;
-                }
-                else
-                {
-                    currentZ += 350f;
-                }
-
-                Instantiate(villageChunk, new Vector3(-18f, 1f, currentZ), Quaternion.Euler(0, 90, 0));
-                chunks.Add(0);
-                previousChunk = "village";
+                GenerateVillage();
             }
             //FOREST
             else if (rand == 1)
             {
 
-                if (previousChunk == "river")
-                {
-                    currentZ += 680;
-                }
-                else
-                {
-                    currentZ += 230;
-                }
-
-                chunks.Add(1);
-                Instantiate(forestChunk, new Vector3(-22f, 0f, currentZ), Quaternion.Euler(0, 90, 0));
-                previousChunk = "forest";
+                GenerateForest();
             }
             //RIVER
             else if (rand == 2)
             {
+                GenerateRiver();
 
-                if (previousChunk == "forest")
-                {
-                    currentZ += 250;
-                    Instantiate(riverChunk, new Vector3(17.57f, 3f, currentZ), Quaternion.Euler(0, -100.92f, 0));
-                }
-                else if (previousChunk == "village")
-                {
-                    currentZ += 180;
-                    Instantiate(riverChunk, new Vector3(17.57f, 3f, currentZ), Quaternion.Euler(0, -100.92f, 0));
-
-                }
-                else if (previousChunk == "river")
-                {
-                    currentZ += 650;
-                    Instantiate(riverChunkNoBoat, new Vector3(17.57f, 3f, currentZ), Quaternion.Euler(0, -100.92f, 0));
-                }
-                else
-                {
-                    currentZ = 110f;
-                    Instantiate(riverChunk, new Vector3(17.57f, 3f, currentZ), Quaternion.Euler(0, -100.92f, 0));
-                }
-
-                chunks.Add(2);
-                previousChunk = "river";
             }
 
             //rand = Random.Range(0, 3);
-
             if (rand == 2 && previousChunk == "river")
             {
                 rand = 1;
@@ -109,8 +61,89 @@ public class GenerateChunks : MonoBehaviour
         //spawn village Add 360 every time to Z coord
 
         Debug.Log("Chunks Generated");
+        int count =0;
+        foreach (GameObject item in chunkObjects)
+        {
+            Debug.Log(item.ToString());
+
+            // if(count == 0){
+            //     Destroy(item);
+            // }
+            // count++;
+        }
+        
         gameObject.AddComponent<ObstacleGenerator>();
 
+    }
+
+    private void GenerateVillage(){
+        if (previousChunk == "forest")
+        {
+            currentZ += 450f;
+        }
+        else if (previousChunk == "river")
+        {
+            currentZ += 850;
+        }
+        else
+        {
+            currentZ += 350f;
+        }
+
+        GameObject villageClone = Instantiate(villageChunk, new Vector3(-18f, 1f, currentZ), Quaternion.Euler(0, 90, 0));
+        chunkObjects.Add(villageClone);
+
+        chunks.Add(0);
+        previousChunk = "village";
+    }
+
+    private void GenerateForest(){
+        if (previousChunk == "river")
+        {
+            currentZ += 680;
+        }
+        else
+        {
+            currentZ += 230;
+        }
+
+        GameObject forestClone = Instantiate(forestChunk, new Vector3(-22f, 0f, currentZ), Quaternion.Euler(0, 90, 0));
+        chunkObjects.Add(forestClone);
+
+        
+        chunks.Add(1);  
+        previousChunk = "forest";
+    }
+
+    private void GenerateRiver(){
+
+        GameObject riverClone;
+        
+
+        if (previousChunk == "forest")
+        {
+            currentZ += 250;
+            riverClone = Instantiate(riverChunk, new Vector3(17.57f, 3f, currentZ), Quaternion.Euler(0, -100.92f, 0));
+        }
+        else if (previousChunk == "village")
+        {
+            currentZ += 180;
+            riverClone = Instantiate(riverChunk, new Vector3(17.57f, 3f, currentZ), Quaternion.Euler(0, -100.92f, 0));
+
+        }
+        else if (previousChunk == "river")
+        {
+            currentZ += 650;
+            riverClone = Instantiate(riverChunkNoBoat, new Vector3(17.57f, 3f, currentZ), Quaternion.Euler(0, -100.92f, 0));
+        }
+        else
+        {
+            currentZ = 110f;
+            riverClone = Instantiate(riverChunk, new Vector3(17.57f, 3f, currentZ), Quaternion.Euler(0, -100.92f, 0));
+        }
+        chunkObjects.Add(riverClone);
+        chunks.Add(2);
+        previousChunk = "river";
     }
 
     public ArrayList GetChunkOrder()
